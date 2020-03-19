9ceecb7e901361e09996390e3223ebf7c109501e
Svetlana <svezan@mail.ru>
2020-03-19 15:21:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }
+  let min = 0;
+  for (let  i = 0; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i]
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }
+  let max = 0;
+  for (let  i = 0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i]
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+  }
+  let result = array.reduce((sum, current) => sum + current, 0);
+  return result / array.length;
 }
